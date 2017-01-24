2089e949fd69f80fad6f0a06de04c5b05e215ae9
Maite Gracia <maitegracia223@gmail.com>
2017-01-24 18:06:59 +0000

    Style desktop

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+
+
+$(window).scroll(function (e) {
+    e.preventDefault();
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    console.log(scroll);
+    console.log(windowHeight);
+    if (scroll > windowHeight) {
+        console.log('ieeeee');
+        $('#Layer_13 .st0, #Layer_12 .st0, #Layer_11 .st0').css('stroke-dashoffset', '1000');
+    }
+});
